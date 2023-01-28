e321e61ed3f1bd0308eece3fc819c1993bf77ffe
Akhil Anilkumar <akhil.slipknot@hotmail.com>
2023-01-28 12:58:30 +0530

    Spring version upgraded Fixed the Cors issue Upgraded to Spring security 6

--- src/main/java/com/prana/apigateway/config/SecurityConfig.java
+++ src/main/java/com/prana/apigateway/config/SecurityConfig.java
@@ -3,39 +3,37 @@ package com.prana.apigateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
-//        configuration.setAllowedMethods(List.of("GET", "POST","PUT", "DELETE"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //.cors(AbstractHttpConfigurer::disable)
-        return http
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity) {
+        return httpSecurity
                 .csrf().disable()
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .cors(ServerHttpSecurity.CorsSpec::disable)
+                .authorizeExchange(authorize -> authorize.anyExchange().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
     @Bean
-    UserDetailsService userDetailsService() {
-        return username -> new User("admin", "admin", Collections.emptyList());
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
